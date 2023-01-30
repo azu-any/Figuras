e67965eda11be3c65f7c20db34f6147d83602551
Azuany Mila Ceron <69259402+azu-any@users.noreply.github.com>
2023-01-30 11:25:38 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,66 +6,116 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("¿Desea crear un circulo o un cuadrado?");
-		Scanner scan = new Scanner(System.in);
-		String figura = scan.nextLine();
-		
-		if (figura.contains("cuadrado")) {
-			System.out.println("¿Que largo tiene?");
-			String largo = scan.nextLine();
-			Double longitud = Double.valueOf(largo);
-			Cuadrado cuadrado1 = new Cuadrado(longitud);
+		while (true) {
 			
-			System.out.println("¿Que desea obtener la diagonal, perimetro o area?");
-			String input = scan.nextLine();
+			System.out.println("¿Desea crear un circulo o un cuadrado?");
+			System.out.println("Para salir escriba 1.");
+			Scanner scan = new Scanner(System.in);
+			String opcion = scan.nextLine();
 			
-			switch(input) {
+			boolean flag = true;
 			
-				case "perimetro":
-					System.out.println("Perimetro: " + cuadrado1.getPerimetro());
-					break;
-					
-				case "diagonal":
-					System.out.println("Diagonal: " + cuadrado1.getDiagonal());
-					break;
+			switch(opcion) {
 				
-				case "area":
-					System.out.println("Area: " + cuadrado1.getArea());
-					break;
-					
-				default:
-					System.out.println("No se puede hacer eso.");
-					break;
-			}
-		} else if(figura.contains("circulo")) {
-			System.out.println("¿Que radio tiene?");
-			String largo = scan.nextLine();
-			Double radio = Double.valueOf(largo);
-			Circulo circulo1 = new Circulo(radio);
+				case "cuadrado":
+						
+					System.out.println("¿Que largo tiene?");
+								
+					while (scan.hasNext()) {
+						if(scan.hasNextDouble()) {
+							break;
+						} else {
+							System.out.println("Ingresa un valor numerico.");
+							scan.nextLine();
+							continue;
+						}
+					}
+						
+					String largo = scan.nextLine();
+					Double longitud = Double.valueOf(largo);
+					Cuadrado cuadrado1 = new Cuadrado(longitud);
+						
+					while (flag) {
+						System.out.println("¿Que desea obtener la diagonal, perimetro o area?");
+						String input = scan.nextLine();
 			
-			System.out.println("¿Que desea obtener la circunferencia o area?");
-			String input = scan.nextLine();
-			
-			switch(input) {
-			
-				case "circunferencia":
-					System.out.println("Circunferencia: " + circulo1.getCircunferencia());
-					break;
-					
-				case "area":
-					System.out.println("Area: " + circulo1.getArea());
-					break;
-					
-				default:
-					System.out.println("No se puede hacer eso.");
-					break;
-			}
-		} else {
-			System.out.println("No se puede hacer eso.");
-		}
+						switch(input) {
 		
-		scan.close();
-
-	}
+							case "perimetro":
+								System.out.println("Perimetro: " + cuadrado1.getPerimetro() + "\n");
+								flag = false;
+								break;
+					
+							case "diagonal":
+								System.out.println("Diagonal: " + cuadrado1.getDiagonal() + "\n");
+								flag = false;
+								break;
+			
+							case "area":
+								System.out.println("Area: " + cuadrado1.getArea() + "\n");
+								flag = false;
+								break;
+				
+							default:
+								System.out.println("No se puede hacer eso. Eliga otra opcion.\n");
+								continue;
+						}
+					}
+				continue;
+						
+			 	case "circulo":
 	
+			 		System.out.println("¿Que radio tiene?");
+			 		
+			 		while (scan.hasNext()) {
+						if(scan.hasNextDouble()) {
+							break;
+						} else {
+							System.out.println("Ingresa un valor numerico.");
+							scan.nextLine();
+							continue;
+						}
+					}
+			 			
+				 	String largo2 = scan.nextLine();
+				 	Double radio = Double.valueOf(largo2);
+				 	Circulo circulo1 = new Circulo(radio);
+				 	
+				 	while(flag) {
+					
+				 		System.out.println("¿Que desea obtener la circunferencia o area?");
+				 		String input2 = scan.nextLine();
+			
+			 			switch(input2) {
+			
+			 				case "circunferencia":
+			 					System.out.println("Circunferencia: " + circulo1.getCircunferencia() + "\n");
+			 					flag = false;
+			 					break;
+					
+			 			case "area":
+			 					System.out.println("Area: " + circulo1.getArea() + "\n");
+			 					flag = false;
+			 					break;
+					
+			 			default:
+			 					System.out.println("No se puede hacer eso. Eliga otra opcion.\n");
+			 					continue;
+			 			}
+			 		}
+				 continue;
+			 		
+			 	case "1":
+			 		System.out.println("Adios.");
+			 		System.exit(0);
+			 		break;
+			 	
+			 	default:
+			 		System.out.println("Opcion incorrecta. Ingrese de nuveo su respuesta");
+			 		continue;
+			}
+		
+			scan.close();
+		}
+	}
 }
